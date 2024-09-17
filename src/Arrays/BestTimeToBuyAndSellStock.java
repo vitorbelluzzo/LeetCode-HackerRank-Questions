9cfe6fbb2de70144e82e5e36d07d4749f595fa79
vitorbelluzzo[ <vitorbelluzzo@hotmail.com>
2024-09-17 00:00:03 -0300

    first commit

---
+++ src/Arrays/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        int valorMinimo = Integer.MAX_VALUE;
+        int lucroMaximo = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < valorMinimo) {
+                valorMinimo = prices[i];
+            } else if (prices[i] - valorMinimo > lucroMaximo) {
+                lucroMaximo = prices[i] - valorMinimo;
+            }
+        }
+        return lucroMaximo;
+    }
+}
---
+++ src/Arrays/TwoSum.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    nums = new int[]{i, j};
+                }
+            }
+        }
+
+        return nums;
+    }
+}
+
+
