3bec2f9e4055d287367ea37ab851e5b15071b359
vitorbelluzzo[ <vitorbelluzzo@hotmail.com>
2024-09-18 16:16:25 -0300

    ContainsDuplicate done

---
+++ src/Arrays/ContainsDuplicate.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> visto = new HashSet<>();
+        for (int num : nums) {
+            if (visto.contains(num))
+                return true;
+            visto.add(num);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ContainsDuplicate containsDuplicate = new ContainsDuplicate();
+        System.out.println(containsDuplicate.containsDuplicate(new int[]{1, 2, 3, 2}));
+    }
+
+
+}
