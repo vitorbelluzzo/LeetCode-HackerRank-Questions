ca1a16586e8a6fad1fd4c11c3462a7805caf8600
vitorbelluzzo[ <vitorbelluzzo@hotmail.com>
2024-10-21 23:33:24 -0300

    day4 solved

---
+++ src/HackerRank/ThirtyDaysOfCode/Day3IntroToConditionalStatements.java
@@ -0,0 +1,25 @@
+package HackerRank.ThirtyDaysOfCode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Day3IntroToConditionalStatements {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        if (N % 2 == 1) {
+            System.out.println("Weird");
+        } else if ( N >= 6 && N <= 20 ){
+            System.out.println("Weird");
+        } else if (N >= 2 && N <= 5 || N > 20) {
+            System.out.println("Not Weird");
+        }
+
+        bufferedReader.close();
+    }
+
+}
+
---
+++ src/HackerRank/ThirtyDaysOfCode/Day4ClassVsInstance.java
@@ -0,0 +1,49 @@
+package HackerRank.ThirtyDaysOfCode;
+
+import java.util.Scanner;
+
+public class Day4ClassVsInstance {
+    private int age;
+
+    public Day4ClassVsInstance(int initialAge) {
+        if (initialAge >= 0) {
+            this.age = initialAge;
+        } else {
+            this.age = 0;
+            System.out.println("Age is not valid, setting age to 0.");
+        }
+    }
+
+    public void amIOld() {
+
+        if (age < 13) {
+            System.out.println("You are young.");
+        } else if (age >= 13 && age < 18) {
+            System.out.println("You are a teenager.");
+        } else {
+            System.out.println("You are old.");
+        }
+
+    }
+
+    public void yearPasses() {
+
+        age++;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int age = sc.nextInt();
+            Day4ClassVsInstance p = new Day4ClassVsInstance(age);
+            p.amIOld();
+            for (int j = 0; j < 3; j++) {
+                p.yearPasses();
+            }
+            p.amIOld();
+            System.out.println();
+        }
+        sc.close();
+    }
+}
